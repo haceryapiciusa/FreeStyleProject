d609a61e7d07d31c9092353c9a1f343b328698f0
haceryapiciusa <56479407+haceryapiciusa@users.noreply.github.com>
2020-02-24 16:17:09 -0500

    neww

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,19 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    @Test
+    public void passingTest(){
+        System.out.println("This test will always pass!");
+    }
+
+//    @Test
+//    public void secondTest(){
+//        System.out.println("never fail test!");
+//    }
+
+//    @Test
+//    public void failingTest(){
+//        Assert.fail();
+//    }
+}
